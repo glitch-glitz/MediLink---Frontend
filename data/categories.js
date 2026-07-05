c4cc4f007f801f90b418ced23f02668fb4c8d96a
Helix <ajangome@yahoo.com>
2026-07-05 13:34:44 +0300

    Added Mock Categories

---
+++ data/categories.js
@@ -0,0 +1,34 @@
+const categories = [
+  {
+    id: 1,
+    title: "Diagnostics",
+    total: 42,
+  },
+  {
+    id: 2,
+    title: "Hospital Furniture",
+    total: 18,
+  },
+  {
+    id: 3,
+    title: "Laboratory Equipment",
+    total: 25,
+  },
+  {
+    id: 4,
+    title: "Surgical Instruments",
+    total: 31,
+  },
+  {
+    id: 5,
+    title: "PPE",
+    total: 56,
+  },
+  {
+    id: 6,
+    title: "Medical Consumables",
+    total: 74,
+  },
+];
+
+export default categories;
