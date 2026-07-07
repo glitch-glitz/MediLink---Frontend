26940883cd1622d9a64944be613d87cd95aae8de
Helix <ajangome@yahoo.com>
2026-07-07 04:15:13 +0300

    Updated product column

--- src/components/product/ProductGrid.jsx
+++ src/components/product/ProductGrid.jsx
@@ -2,7 +2,7 @@ import ProductCard from "./ProductCard";
 
 const ProductGrid = ({ products }) => {
   return (
-    <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-6">
+    <div className="grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-5 xl:grid-cols-6 gap-4">
       {products.map((product) => (
         <ProductCard
           key={product.id}
